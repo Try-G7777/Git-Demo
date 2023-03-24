b0f2ee421b250ec3105999f212ac26e5d1f9e047
Zs <1582460701@qq.com>
2023-03-24 17:00:42 +0800

    test分支修改2

--- demo-module-1/src/com/duyi/MainTest3.java
+++ demo-module-1/src/com/duyi/MainTest3.java
@@ -2,7 +2,6 @@ package com.duyi;
 
 public class MainTest3 {
     public static void main(String[] args) {
-        System.out.println("hello welcome");
-        System.out.println("push push");
+
     }
 }
