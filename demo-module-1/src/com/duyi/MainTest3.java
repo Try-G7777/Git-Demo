ca21ab2f195be187a54b6021e03c5280769364a0
Zs <1582460701@qq.com>
2023-03-24 16:31:00 +0800

    嘿嘿嘿嘿

--- demo-module-1/src/com/duyi/MainTest3.java
+++ demo-module-1/src/com/duyi/MainTest3.java
@@ -3,5 +3,6 @@ package com.duyi;
 public class MainTest3 {
     public static void main(String[] args) {
         System.out.println("hello welcome");
+        System.out.println("push push");
     }
 }
