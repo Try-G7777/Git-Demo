17dd81b9dc1b02d6f871b8ab5dd9e0b8bd3ec35b
Zs <1582460701@qq.com>
2023-03-24 16:55:53 +0800

    test分支修改

--- demo-module-1/src/com/duyi/MainTest2.java
+++ demo-module-1/src/com/duyi/MainTest2.java
@@ -1,9 +1,4 @@
 package com.duyi;
 
 public class MainTest2 {
-    public static void main(String[] args) {
-        System.out.println("hello git");
-
-        System.out.println("test change");
-    }
 }
